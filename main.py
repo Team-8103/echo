d7283080c111f1cefe6ce2cf58847bcbfdd04364
Aadarsh Padiyath <aspadiyath@gmail.com>
2018-10-23 11:57:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('GTHousingHVACandLighting')
 
+
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
     print("event.session.application.applicationId=" +
-        event['session']['application']['applicationId'])
+          event['session']['application']['applicationId'])
 
     """
     Uncomment this if statement and populate with your skill's application ID to
     prevent someone else from configuring a skill that sends requests to this
     function.
     """
-    
+
     if (event['session']['application']['applicationId'] !=
             "[REDACTED]"):
         raise ValueError("Invalid Application ID")
@@ -26,7 +27,7 @@ def lambda_handler(event, context):
                            event['session'])
 
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        return on_launch(event, event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
@@ -40,15 +41,15 @@ def on_session_started(session_started_request, session):
           + ", sessionId=" + session['sessionId'])
 
 
-def on_launch(launch_request, session):
+def on_launch(event, session):
     """ Called when the user launches the skill without specifying what they
     want
     """
 
-    print("on_launch requestId=" + launch_request['requestId'] +
+    print("on_launch requestId=" + event['request']['requestId'] +
           ", sessionId=" + session['sessionId'])
     # Dispatch to your skill's launch
-    return get_welcome_response()
+    return get_welcome_response(event['request'], event['session'])
 
 
 def on_intent(intent_request, session):
@@ -62,11 +63,11 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
     if intent_name == "ChangeIntent":
-        return changeIntent(intent, session)
+        return change_intent(intent)
     elif intent_name == "StateIntent":
-        return stateIntent(intent, session)
+        return state_intent(intent)
     elif intent_name == "LightsIntent":
-        return lightsIntent(intent, session)
+        return lights_intent(intent)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response(intent, session)
     elif intent_name == "AMAZON.CancelIntent":
@@ -84,14 +85,14 @@ def on_session_ended(session_ended_request, session):
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
     # add cleanup logic here
-    
-    
-def getHouse():
+
+
+def get_house():
     table2 = dynamodb.Table('GTIDtoRoomNumber')
     item = table2.get_item(
         Key={
             "GTID": "901234567"
-            })
+        })
     room_number = item.get('Item').get('Room_Number')
     return room_number[:-1]
 
@@ -103,7 +104,7 @@ def get_welcome_response(intent, session):
     """ If we wanted to initialize the session to have some attributes we could
     add those here
     """
-    session_attributes = getAttributes(intent, session)
+    session_attributes = get_attributes(session)
     card_title = "Welcome"
     speech_output = "Welcome to Georgia Tech Housing's Alexa Skill. " \
                     "Please tell me what temperature you want this room to be " \
@@ -119,7 +120,7 @@ def get_welcome_response(intent, session):
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def changeIntent(intent, session):
+def change_intent(intent):
     """ Sets the color in the session and prepares the speech to reply to the
     user.
     """
@@ -131,7 +132,7 @@ def changeIntent(intent, session):
         if 'value' not in intent['slots']['temperature']:
             speech_output = "I'm having trouble hearing you right now"
             return build_response(session_attributes, build_speechlet_response(
-            card_title, speech_output, None, False))
+                card_title, speech_output, None, False))
         else:
             temperature = intent['slots']['temperature']['value']
         if int(temperature) < 90:
@@ -141,11 +142,11 @@ def changeIntent(intent, session):
                                 temperature + " degrees."
                 table.update_item(
                     Key={
-                        "Building_RoomNumber": getHouse()
+                        "Building_RoomNumber": get_house()
                     },
                     AttributeUpdates={
                         "HVAC": {
-                            "Action": "PUT", 
+                            "Action": "PUT",
                             "Value": int(temperature)
                         }
                     })
@@ -154,15 +155,15 @@ def changeIntent(intent, session):
                 temperature = "50"
                 session_attributes["temperature"] = temperature
                 speech_output = "The minimum temperature allowed is" \
-                " 50 degrees. Changing the temperature to " + \
-                            temperature + " degrees."
+                                " 50 degrees. Changing the temperature to " + \
+                                temperature + " degrees."
                 table.update_item(
                     Key={
-                        "Building_RoomNumber": getHouse()
+                        "Building_RoomNumber": get_house()
                     },
                     AttributeUpdates={
                         "HVAC": {
-                            "Action": "PUT", 
+                            "Action": "PUT",
                             "Value": int(temperature)
                         }
                     })
@@ -170,19 +171,19 @@ def changeIntent(intent, session):
         else:
             temperature = "90"
             session_attributes["temperature"] = temperature
-            speech_output = "The maximum temperature allowed is"\
-            " 90 degrees. Changing the temperature to " + \
-                        temperature + " degrees."
+            speech_output = "The maximum temperature allowed is" \
+                            " 90 degrees. Changing the temperature to " + \
+                            temperature + " degrees."
             table.update_item(
-                    Key={
-                        "Building_RoomNumber": getHouse()
-                    },
-                    AttributeUpdates={
-                        "HVAC": {
-                            "Action": "PUT", 
-                            "Value": int(temperature)
-                        }
-                    })
+                Key={
+                    "Building_RoomNumber": get_house()
+                },
+                AttributeUpdates={
+                    "HVAC": {
+                        "Action": "PUT",
+                        "Value": int(temperature)
+                    }
+                })
             reprompt_text = None
     else:
         speech_output = "I'm not sure what you are asking. " \
@@ -191,8 +192,9 @@ def changeIntent(intent, session):
                         "Please try again."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
-        
-def lightsIntent(intent, session):
+
+
+def lights_intent(intent):
     """ Sets the light in the session and prepares the speech to reply to the
     user.
     """
@@ -211,15 +213,15 @@ def lightsIntent(intent, session):
         else:
             power = False
         table.update_item(
-                    Key={
-                        "Building_RoomNumber": getHouse()
-                    },
-                    AttributeUpdates={
-                        "Lighting": {
-                            "Action": "PUT", 
-                            "Value": power
-                        }
-                    })
+            Key={
+                "Building_RoomNumber": get_house()
+            },
+            AttributeUpdates={
+                "Lighting": {
+                    "Action": "PUT",
+                    "Value": power
+                }
+            })
         reprompt_text = None
     else:
         speech_output = "I'm not sure what you are asking. " \
@@ -231,32 +233,35 @@ def lightsIntent(intent, session):
 
 def create_temperature_attribute(temperature):
     return {"temperature": temperature}
-    
+
+
 def create_power_attribute(power):
     return {"power": power}
 
-def getAttributes(intent, session):
-    sessionAttributes = {}
+
+def get_attributes(session):
+    session_attributes = {}
     if "temperature" in session.get('attributes', {}):
         temperature = session['attributes']['temperature']
-        sessionAttributes["temperature"] = temperature
+        session_attributes["temperature"] = temperature
     if "power" in session.get('attributes', {}):
         power = session['attributes']['power']
-        sessionAttributes["power"] = power
-    return sessionAttributes
-        
-def stateIntent(intent, session):
+        session_attributes["power"] = power
+    return session_attributes
+
+
+def state_intent(intent):
     session_attributes = {}
     reprompt_text = None
-    
+
     item = table.get_item(
         Key={
-            "Building_RoomNumber": getHouse()
-            })
-    
+            "Building_RoomNumber": get_house()
+        })
+
     temperature = item.get('Item').get('HVAC')
-    
-    if temperature != None:
+
+    if temperature is not None:
         speech_output = "Your current temperature is " + str(temperature) + \
                         " degrees."
         should_end_session = True
