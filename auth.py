3d7a5f79075adc9172b1a2437a23ea4bc5797e97
Aadarsh Padiyath <aspadiyath@gmail.com>
2018-11-29 17:11:12 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,71 +1,71 @@
 import logging
 import uuid
 import urllib3
-import xml.etree.ElementTree as ET
 import boto3
-import redirect
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-baseServiceUrl = "https://hvac.aws.itg.gatech.edu/auth/login"
+baseServiceUrl = "https://housinghvac.itg.gatech.edu/gatechSSOauth"
 baseCasUrl = "https://login.gatech.edu/cas/"
 
-def auth_lambda_handler(event, context):
+
+def handler(event, context):
     """
-    Lambda handler for authentication.
-    Retrieves the username and token from the event data. Will throw an exception
-    if the event parameters are invalid or missing.
-    @param: event Dictionary of event parameters passed to the lambda function.
+    Handles authorization of a GT student.
+    @param: event Dictionary of event parameters.
+    @param: context The context of the call.
     """
-    _returnUrl = None;
-    _ticket = None;
-    _state = None;
-    _redirect_uri = None;
+    _returnUrl = None
+    _ticket = None
+    _state = None
+    _redirect_uri = "https://pitangui.amazon.com/spa/skill/account-linking-status.html?vendorId=MV8VM7SY8W1S6"
 
     logger.info("Invoked")
     logger.info("Alexalogin")
     logger.info("event=" + str(event))
 
     # Check for returnUrl or redirect_uri
-    if 'returnUrl' in event:
-        _returnUrl = event['returnUrl']
+    if 'queryStringParameters' in event:
+        # if 'returnUrl' in event['queryStringParameters']:
+        #     _returnUrl = event['queryStringParameters']['returnUrl']
 
-    if 'redirect_uri' in event:
-        _redirect_uri = event['redirect_uri']
+        if 'redirect_uri' in event['queryStringParameters']:
+            _redirect_uri = event['queryStringParameters']['redirect_uri']
+
+        if 'state' in event['queryStringParameters']:
+            _state = event['queryStringParameters']['state']
+
+        if 'ticket' in event['queryStringParameters']:
+            logger.info("Ticket is in Event")
+            _ticket = event['queryStringParameters']['ticket']
 
     if _returnUrl is None and _redirect_uri is None:
-        return _raise_exception("returnUrl or redirect_uri is required")
+        return raise_exception("returnUrl or redirect_uri is required")
 
     logger.info("serviceUrl=" + baseServiceUrl)
-    logger.info("returnUrl=" + _returnUrl)
+    # logger.info("returnUrl=" + _returnUrl)
     logger.info("redirect_uri=" + _redirect_uri)
 
-    # Check for state and ticket
-    if 'state' in event:
-        _state = event['state']
-
-    if 'ticket' in event:
-        _ticket = event['ticket']
-
     if _ticket is None:
-        return redirect.to_cas(_state, _redirect_uri)
+        return redirect_to_cas(_state, _redirect_uri)
     logger.info("ticket=" + _ticket)
 
     # Validate ticket
-    gtUsername = _validate_ticket(_ticket, _state, _redirect_uri)
+    gtUsername = validate_ticket(_ticket, _state, _redirect_uri)
     if gtUsername is None:
-        return _raise_exception("Unable to validate ticket.")
+        return raise_exception("Unable to validate ticket.")
 
     # Store username/token pair
     token = str(uuid.uuid4())
-    if not _write_record(gtUsername, token):
-        return _raise_exception("Unable to store token.")
+    if not write_record(gtUsername, token):
+        return raise_exception("Unable to store token.")
 
     # Return to Alexa
-    return redirect.to_alexa(token, _state, _redirect_uri)
+    return return_to_alexa(token, _redirect_uri, _state)
 
-def _raise_exception(message, log_error=True):
+
+def raise_exception(message, log_error=True):
     """
     Raises an exception and (optionally)
     logs it using the logger.
@@ -74,7 +74,8 @@ def _raise_exception(message, log_error=True):
         logger.error(message)
     raise Exception(message)
 
-def _validate_ticket(ticket, state, redirect_uri):
+
+def validate_ticket(ticket, state, redirect_uri):
     """
     Gets the GT Username from the ticket.
     @return: The username associated with the ticket.
@@ -89,24 +90,25 @@ def _validate_ticket(ticket, state, redirect_uri):
 
     user = None
     try:
-        xml = _get_response(validateUrl)
+        xml = get_response(validateUrl)
         logger.info("validate_ticket xml=" + xml.replace("\n", "\t"))
-        user = _get_username_from_xml(xml)
+        user = get_username(xml)
         logger.info("user=" + user)
     except Exception as e:
         logger.error("Unable to validate ticket.")
         logger.error("Error: " + str(e))
-    
+
     return user
 
-def _write_record(user, token):
+
+def write_record(user, token):
     """
     Writes the username/token pair to a database.
     @return: True if successful, false otherwise.
     """
     try:
-        access_key = "" #TODO: Get access_key
-        secret_key = "" #TODO: Get secret_key
+        access_key = "AKIAJF273WN5WJ2UIGOA"
+        secret_key = "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"
         client = boto3.client('dynamodb',
                               region_name='us-east-1',
                               aws_access_key_id=access_key,
@@ -121,10 +123,44 @@ def _write_record(user, token):
     except Exception as e:
         logger.error("Unable to write record.")
         logger.error("Error: " + str(e))
-    
+
     return False
 
-def _get_response(url, decode_type='utf-8'):
+
+def redirect_to_cas(state, redirect_uri):
+    """
+    Returns a dictionary that will redirect the service
+    to CAS in order to login.
+    """
+    logger.info("Redirecting to CAS. No ticket.")
+
+    serviceUrl = baseServiceUrl + \
+        "?state=" + state + \
+        "&redirect_uri=" + redirect_uri
+
+    loginUrl = baseCasUrl + "login?service=" + serviceUrl
+    logger.info("loginUrl=" + loginUrl)
+
+    return {'statusCode': 302, 'headers': {
+        'Location': loginUrl}}
+
+
+def return_to_alexa(token, redirect_uri, state):
+    """
+    Returns a dictionary that will redirect the service
+    to the Alexa app.
+    """
+    redirect_uri = redirect_uri + \
+        "#state=" + state + \
+        "&access_token=" + token + \
+        "&token_type=Bearer"
+    logger.info("redirect_uri=" + redirect_uri)
+
+    return {'statusCode': 302, 'headers': {
+        'Location': redirect_uri}}
+
+
+def get_response(url, decode_type='utf-8'):
     """
     Returns the string resulting from a GET
     request to the provided url.
@@ -133,18 +169,15 @@ def _get_response(url, decode_type='utf-8'):
     response = http.request("GET", url)
     return response.data.decode(decode_type)
 
-def _get_username_from_xml(xml):
+
+def get_username(xml):
     """
     Attempts to get the username from the XML string.
     @return: The username if found, None otherwise.
     """
-    root = ET.fromstring(xml)
     user = None
     try:
-        a = 'cas:serviceResponse'
-        b = 'cas:authenticationSuccess'
-        c = 'cas:user'
-        user = root.find(a).find(b).find(c)
+        user = xml.split("cas:user")[1][1:-2]
     except Exception as e:
         logger.error("Unable to parse XML.")
         logger.error("Error: " + str(e))
